b6248e0aa99d0aa6b65d372ab0ef9cdb4353831d
deto <jorgerubilarssj@gmail.com>
2025-11-15 21:38:35 -0300

    added relations to Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,4 +14,16 @@ class Product extends Model
         'image_url',
         'available'
     ];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems() {
+        return $this->hasMany(OrderItems::class);
+    }
+
+    public function cartItems() {
+        return $this->hasMany(CartItems::class);
+    }
 }
