84a90ed82acd2b7ef947b2b351b13dc86d1ba2c1
Jorge Rubilar <jorgerubilarssj@gmail.com>
2025-11-17 13:04:18 -0300

    changed Example.jsx

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -1,30 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
 
-function Example() {
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+const Example = () => {
+  return (
+    <div>Example</div>
+  )
 }
 
-export default Example;
-
-if (document.getElementById('example')) {
-    const Index = ReactDOM.createRoot(document.getElementById("example"));
-
-    Index.render(
-        <React.StrictMode>
-            <Example/>
-        </React.StrictMode>
-    )
-}
+export default Example
