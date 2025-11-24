94c46f653e1fe43aa340c98d58ef8a2e8eb88a2d
detossj <jorgerubilarssj@gmail.com>
2025-11-24 19:02:43 -0300

    added Navbar to LayoutPublic.jsx

--- resources/js/layouts/LayoutPublic.jsx
+++ resources/js/layouts/LayoutPublic.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import TopBar from '../components/TopBar'
 import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const LayoutPublic = () => {
   return (
     <>
       <TopBar/>
+      <Navbar/>
         <Outlet/>
       <Footer/>
     </>
