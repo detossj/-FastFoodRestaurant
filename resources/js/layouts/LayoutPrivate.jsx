6dbb544dbbd11fbd3472c424a763d7c2985ddcee
detossj <jorgerubilarssj@gmail.com>
2025-11-24 19:02:49 -0300

    added Navbar to LayoutPrivate.jsx

--- resources/js/layouts/LayoutPrivate.jsx
+++ resources/js/layouts/LayoutPrivate.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Footer from '../components/Footer'
 import TopBar from '../components/TopBar'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const LayoutPrivate = () => {
   return (
     <>
       <TopBar/>
+      <Navbar/>
         <Outlet/>
       <Footer/>
     </>
