61e36c8d1066a0a835187862c5972d89127e3399
detossj <jorgerubilarssj@gmail.com>
2025-11-26 13:56:09 -0300

    changed NavBar.jsx design

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -3,37 +3,47 @@ import React from 'react'
 const Navbar = () => {
   return (
 
-    <div className="row justify-content-center mt-4">
-       <div className="col-12 col-md-8 col-lg-8">
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">INICIO</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">PROMOCIONES</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">PIZZAS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">HAMBURGESAS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">ACOMPANAMIENTOS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">BEBIDAS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">POSTRES</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">EXTRAS</a>
-                        </li>
-                        <li class="nav-item dropdown">
+    <div className="py-2" style={{borderBottom: "1px solid #e5e5e5"}}>
+        <div className="container">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+
+                    <li className="nav-item">
+                    <a className="nav-link active" href="#">INICIO</a>
+                    </li>
+
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">PROMOCIONES</a>
+                    </li>
+
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">PIZZAS</a>
+                    </li>
+
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">HAMBURGESAS</a>
+                    </li>
+
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">ACOMPAÑAMIENTOS</a>
+                    </li>
+
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">BEBIDAS</a>
+                    </li>
+
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">POSTRES</a>
+                    </li>
+
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">EXTRAS</a>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Dropdown link
                             </a>
@@ -43,12 +53,14 @@ const Navbar = () => {
                             <li><a class="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                         </li>
-                        </ul>
-                    </div>
+                </ul>
                 </div>
+
+            </div>
             </nav>
         </div>
     </div>
+
   )
 }
 
