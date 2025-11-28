f364fccfc11815328cd911d44ff09281c0ac3b2a
detossj <jorgerubilarssj@gmail.com>
2025-11-28 13:41:20 -0300

    changed TopBar.jsx and added NavLinks and import TopBar.css

--- resources/js/components/TopBar.jsx
+++ resources/js/components/TopBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../../assets/images/logo.webp';
-
+import './TopBar.css';
+import { NavLink } from 'react-router-dom';
 const TopBar = () => {
   return (
     <div className="py-2" style={{borderBottom: "1px solid #e5e5e5"}}>
@@ -12,21 +13,41 @@ const TopBar = () => {
 
           <div className="d-flex align-items-center gap-4">
 
-            <div className="topbar-item">
-              <i className="fa-solid fa-pizza-slice me-1" style={{color: 'orange'}}></i> MENÚ
-            </div>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "topbar-item active" : "topbar-item"
+              }
+            >
+              <i className="fa-solid fa-pizza-slice me-1"></i> MENÚ
+            </NavLink>
 
-            <div className="topbar-item">
-              <i className="fa-solid fa-receipt me-1" style={{color: 'orange'}}></i> PEDIDOS
-            </div>
+            <NavLink
+              to="/pedidos"
+              className={({ isActive }) =>
+                isActive ? "topbar-item active" : "topbar-item"
+              }
+            >
+              <i className="fa-solid fa-receipt me-1"></i> PEDIDOS
+            </NavLink>
 
-            <div className="topbar-item">
-              <i className="fa-solid fa-store me-1" style={{color: 'orange'}}></i> LOCALES
-            </div>
+            <NavLink
+              to="/locales"
+              className={({ isActive }) =>
+                isActive ? "topbar-item active" : "topbar-item"
+              }
+            >
+              <i className="fa-solid fa-store me-1"></i> LOCALES
+            </NavLink>
 
-            <div className="topbar-item">
-              <i className="fa-solid fa-user me-1" style={{color: 'orange'}}></i> INGRESAR
-            </div>
+            <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                isActive ? "topbar-item active" : "topbar-item"
+              }
+            >
+              <i className="fa-solid fa-user me-1"></i> INGRESAR
+            </NavLink>
 
           </div>
 
