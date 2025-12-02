ea075a8d2c058f76a3f0ef33f5d2ea1dc8c04d07
detossj <jorgerubilarssj@gmail.com>
2025-12-02 19:08:04 -0300

    changed Navbar.jsx

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -1,68 +1,112 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './NavBar.css';
 
 const Navbar = () => {
   return (
-
     <div className="py-2" style={{borderBottom: "1px solid #e5e5e5"}}>
-        <div className="container">
-            <nav className="navbar navbar-expand-lg ">
-            <div className="container-fluid">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
 
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
 
-                    <li className="nav-item">
-                    <a className="nav-link active" href="#">INICIO</a>
-                    </li>
+                <li className="nav-item">
+                  <NavLink 
+                    to="/" 
+                    className={({ isActive }) =>
+                      isActive ? "navbar-item active" : "navbar-item"
+                    }
+                  >
+                    INICIO
+                  </NavLink>
+                </li>
 
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">PROMOCIONES</a>
-                    </li>
+                <li className="nav-item">
+                  <NavLink 
+                    to="/promociones" 
+                    className={({ isActive }) =>
+                      isActive ? "navbar-item active" : "navbar-item"
+                    }
+                  >
+                    PROMOCIONES
+                  </NavLink>
+                </li>
 
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">PIZZAS</a>
-                    </li>
+                <li className="nav-item">
+                  <NavLink 
+                    to="/pizzas" 
+                    className={({ isActive }) =>
+                      isActive ? "navbar-item active" : "navbar-item"
+                    }
+                  >
+                    PIZZAS
+                  </NavLink>
+                </li>
 
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">HAMBURGESAS</a>
-                    </li>
+                <li className="nav-item">
+                  <NavLink 
+                    to="/hamburguesas" 
+                    className={({ isActive }) =>
+                      isActive ? "navbar-item active" : "navbar-item"
+                    }
+                  >
+                    HAMBURGUESAS
+                  </NavLink>
+                </li>
 
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">ACOMPAÑAMIENTOS</a>
-                    </li>
+                <li className="nav-item">
+                  <NavLink 
+                    to="/acompanamientos" 
+                    className={({ isActive }) =>
+                      isActive ? "navbar-item active" : "navbar-item"
+                    }
+                  >
+                    ACOMPAÑAMIENTOS
+                  </NavLink>
+                </li>
 
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">BEBIDAS</a>
-                    </li>
+                <li className="nav-item">
+                  <NavLink 
+                    to="/bebidas" 
+                    className={({ isActive }) =>
+                      isActive ? "navbar-item active" : "navbar-item"
+                    }
+                  >
+                    BEBIDAS
+                  </NavLink>
+                </li>
 
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">POSTRES</a>
-                    </li>
+                <li className="nav-item">
+                  <NavLink 
+                    to="/postres" 
+                    className={({ isActive }) =>
+                      isActive ? "navbar-item active" : "navbar-item"
+                    }
+                  >
+                    POSTRES
+                  </NavLink>
+                </li>
 
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">EXTRAS</a>
-                    </li>
-                </ul>
-                <ul className="navbar-nav ms-auto">
-                  <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown link
-                    </a>
-                    <ul className="dropdown-menu">
-                      <li><a className="dropdown-item" href="#">Action</a></li>
-                      <li><a className="dropdown-item" href="#">Another action</a></li>
-                      <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </li>
-                </ul>
-                
-                </div>
+                <li className="nav-item">
+                  <NavLink 
+                    to="/extras" 
+                    className={({ isActive }) =>
+                      isActive ? "navbar-item active" : "navbar-item"
+                    }
+                  >
+                    EXTRAS
+                  </NavLink>
+                </li>
 
+              </ul>
             </div>
-            </nav>
-        </div>
-    </div>
 
+          </div>
+        </nav>
+      </div>
+    </div>
   )
 }
 
