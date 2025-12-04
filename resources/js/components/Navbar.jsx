1fe58f753e1a7fc8eaa020dcd1912660b5653d68
detossj <jorgerubilarssj@gmail.com>
2025-12-04 15:02:29 -0300

    added logo to NavBar.jsx

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './NavBar.css';
+import logo from '../../assets/images/logo.webp';
 
 const Navbar = () => {
+
+  const [scrolled, setScrolled] = useState(false);
+
+
+  useEffect(() => {
+    // Funcion para que la navbar quede pegada arriba al scrollear
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50); // si baja más de 50px activa el logo
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
-    <div className="py-2" style={{borderBottom: "1px solid #e5e5e5"}}>
+    <div className="p-3 navbar-wrapper" style={{borderBottom: "1px solid #e5e5e5"}}>
       <div className="container">
         <nav className="navbar navbar-expand-lg">
           <div className="container-fluid">
 
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul className="navbar-nav">
+          {scrolled && (
+              <img 
+                style={{ height: '50px', cursor: 'pointer'}}
+                src={logo} 
+                alt="Logo"
+                className="navbar-logo"
+              />
+            )}
 
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className={`navbar-nav ${scrolled ? "with-logo" : ""}`}>
                 <li className="nav-item">
                   <NavLink 
                     to="/" 
