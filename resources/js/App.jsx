0bed96fb12e3658d4b53eb680b040c154833887d
detossj <jorgerubilarssj@gmail.com>
2025-12-07 12:24:53 -0300

    added Routes in App.jsx

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -6,6 +6,13 @@ import LayoutPublic from './layouts/LayoutPublic';
 import Home from './pages/Home';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "../sass/app.scss";
+import Promociones from './pages/Promociones';
+import Pizzas from './pages/Pizzas';
+import Hamburguesas from './pages/Hamburguesas';
+import Acompañamientos from './pages/Acompañamientos';
+import Bebidas from './pages/Bebidas';
+import Postres from './pages/Postres';
+import Extras from './pages/Extras';
 
 
 const App = () => {
@@ -14,7 +21,13 @@ const App = () => {
         <Routes>
             <Route path='/' element={<LayoutPublic/>}>
               <Route index element={<Home/>}/>
-
+              <Route path='/promociones' element={<Promociones/>}/>
+              <Route path='/pizzas' element={<Pizzas/>}/>
+              <Route path='/hamburguesas' element={<Hamburguesas/>}/>
+              <Route path='/acompanamientos' element={<Acompañamientos/>}/>
+              <Route path='/bebidas' element={<Bebidas/>}/>
+              <Route path='/postres' element={<Postres/>}/>
+              <Route path='/extras' element={<Extras/>}/>
             </Route>
         </Routes>
     </Router>
