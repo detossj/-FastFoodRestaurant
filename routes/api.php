a825c7a156c239774da0142864eb17b8df63c331
detossj <jorgerubilarssj@gmail.com>
2025-12-08 14:13:29 -0300

    added Route of ProductController in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+
+// Public
+
+Route::get('category/{id}/products', [ProductController::class,'productsByCategory']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
