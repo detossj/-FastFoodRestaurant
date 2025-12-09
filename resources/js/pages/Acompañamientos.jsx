c2810ac6ab9a29d31b2254d3f2170673b39fe173
detossj <jorgerubilarssj@gmail.com>
2025-12-09 13:20:15 -0300

    changed Acompañamientos.jsx

--- resources/js/pages/Acompañamientos.jsx
+++ resources/js/pages/Acompañamientos.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import ItemsList from '../components/ItemsList'
 
 const Acompañamientos = () => {
   return (
-    <div>Acompañamientos</div>
+    <ItemsList id={3}/>
   )
 }
 
