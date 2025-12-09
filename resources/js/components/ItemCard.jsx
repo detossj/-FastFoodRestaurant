114ae19674b54a0afde0ce24449d188d250b54fe
detossj <jorgerubilarssj@gmail.com>
2025-12-09 17:36:17 -0300

    changed ItemCard.jsx

--- resources/js/components/ItemCard.jsx
+++ resources/js/components/ItemCard.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from 'react';
+import './ItemCard.css';
+import Config from '../Config';
 
-const ItemCard = ({product}) => {
+const ItemCard = ({ product }) => {
   return (
-    <div className='card'>
-        
-        <p>{product.name}</p>
-    </div>
-  )
-}
+    <div className="product-card shadow rounded p-3">
 
-export default ItemCard
+      <div className="product-img-wrapper mb-3">
+        <img 
+          src={`${Config.API_BASE_URL}/storage/products/${product.image_url}`} 
+          alt={product.name} 
+          className="product-img"
+          loading='lazy'
+        />
+      </div>
+
+
+
+
+      <h5 className="fw-bold text-center">{product.name}</h5>
+
+
+      <p className="text-muted text-center small">
+        {product.description ?? 'Sin descripción disponible.'}
+      </p>
+
+
+      <p className="product-price text-center fw-semibold">
+        ${product.price}
+      </p>
+
+
+      <div className="mt-auto text-center">
+        <button className="btn btn-primary px-4" style={{backgroundColor: "rgb(249, 124, 47)", borderColor: 'rgb(249, 124, 47)'}}>Agregar</button>
+      </div>
+    </div>
+  );
+};
+
+export default ItemCard;
