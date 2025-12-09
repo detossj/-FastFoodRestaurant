9a230c7184adab2b7385d3924bed8f99e023655d
detossj <jorgerubilarssj@gmail.com>
2025-12-09 13:19:39 -0300

    created LoadingBar.jsx

---
+++ resources/js/components/LoadingBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LoadingBar = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center" style={{ height: "50vh" }}>
+        <div 
+          className="spinner-border" 
+          role="status" 
+          style={{ width: "6rem", height: "6rem", color:"rgb(249, 124, 47)", borderWidth: "0.4rem"}} 
+        >
+        </div>
+    </div>
+  )
+}
+
+export default LoadingBar
