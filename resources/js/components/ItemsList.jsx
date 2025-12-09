dc0c61d6a97802933cfca293407307a070900082
detossj <jorgerubilarssj@gmail.com>
2025-12-09 13:18:10 -0300

    changed ItemsList.jsx

--- resources/js/components/ItemsList.jsx
+++ resources/js/components/ItemsList.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Config from '../Config';
+import ItemCard from './ItemCard';
+import LoadingBar from './LoadingBar';
 
-const ItemsList = () => {
+const ItemsList = ({id}) => {
+
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const LoadProducts = async () => {
+    try {
+
+      const { data } = await Config.getProductsByCategory(id);
+
+      setProducts(data ?? [])
+
+      setLoading(false);
+
+    } catch (error) {
+
+      console.error('Error al cargar los datos:',error)
+      
+      setProducts([])
+
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    LoadProducts()
+  },[]); 
+
+  if (loading) {
+    return (
+      <LoadingBar/>
+    );
+  }
+
   return (
-    <div>ItemsList</div>
+    <>
+      <div className='container p-5' style={{ minHeight: "100vh" }}>
+        {
+          products.map((product) => {
+            return <ItemCard product={product}/>
+          })
+        }
+      </div>
+    </>
   )
 }
 
