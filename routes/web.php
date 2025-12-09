122d15168219a463483ac0fd8d2db859b94e4425
detossj <jorgerubilarssj@gmail.com>
2025-12-09 13:15:20 -0300

    fixed web.php

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 // Si empieza con /api no se captura y deja que vaya las rutas de la api
 Route::get('{any}', function () {
-    return view('app');
+    return view('welcome');
 })->where('any', '^(?!api).*$');
