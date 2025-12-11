a0045a1e3df6e99cddd873d5070f54441ae6cee5
detossj <jorgerubilarssj@gmail.com>
2025-12-11 13:07:58 -0300

    changed ItemCard.jsx

--- resources/js/components/ItemCard.jsx
+++ resources/js/components/ItemCard.jsx
@@ -7,31 +7,36 @@ const ItemCard = ({ product }) => {
     <div className="product-card shadow rounded p-3">
 
       <div className="product-img-wrapper mb-3">
-      <img 
-        src={`${Config.API_BASE_URL}/storage/products${product.image_url}`}
-        alt={product.name}
-        className="product-img"
-        loading='lazy'
-      />
+        <img 
+          src={`${Config.API_BASE_URL}/storage/products${product.image_url}`}
+          alt={product.name}
+          className="product-img"
+          loading='lazy'
+        />
       </div>
 
-      <h5 className="fw-bold text-center">{product.name}</h5>
+      <div className="product-info mt-2">
+          <h5 className="fw-bold text-center">{product.name}</h5>
 
-
-      <p className="text-muted text-center small">
-        {product.description ?? 'Sin descripción disponible.'}
-      </p>
-
-
-      <p className="product-price text-center fw-semibold">
-        ${product.price}
-      </p>
-
-
-      <div className="mt-auto text-center">
-        <button className="btn btn-primary px-4" style={{backgroundColor: "rgb(249, 124, 47)", borderColor: 'rgb(249, 124, 47)'}}>Agregar</button>
+          <p className="text-muted text-center small">
+            {product.description ?? 'Sin descripción disponible.'}
+          </p>
       </div>
+
+      <div className="product-bottom">
+          <p className="product-price text-center fw-semibold">
+            ${product.price}
+          </p>
+
+          <button className="btn btn-primary px-4" 
+            style={{backgroundColor: "rgb(249, 124, 47)", borderColor: 'rgb(249, 124, 47)'}}
+          >
+            Agregar
+          </button>
+      </div>
+
     </div>
+
   );
 };
 
