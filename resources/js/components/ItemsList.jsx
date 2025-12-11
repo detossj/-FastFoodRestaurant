d339de5ab41c1c589e6b90f1b6304a91396166a9
detossj <jorgerubilarssj@gmail.com>
2025-12-11 18:28:25 -0300

    changed ItemList.jsx

--- resources/js/components/ItemsList.jsx
+++ resources/js/components/ItemsList.jsx
@@ -1,53 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import Config from '../Config';
-import ItemCard from './ItemCard';
-import LoadingBar from './LoadingBar';
+import React from 'react'
+import { useProducts } from "../context/ProductsContext";
+import ItemCard from "./ItemCard";
+import LoadingBar from "./LoadingBar";
 
-const ItemsList = ({id}) => {
+const ItemsList = ({ id }) => {
+  const { products, loading } = useProducts();
 
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  if (loading) return <LoadingBar />;
 
-  const LoadProducts = async () => {
-    try {
-
-      const { data } = await Config.getProductsByCategory(id);
-
-      setProducts(data ?? [])
-
-      setLoading(false);
-
-    } catch (error) {
-
-      console.error('Error al cargar los datos:',error)
-      
-      setProducts([])
-
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    LoadProducts()
-  },[id]); 
-
-  if (loading) {
-    return (
-      <LoadingBar/>
-    );
-  }
+  const filtered = products.filter(p => p.category_id === id);
 
   return (
-    <>
-      <div className="container p-5" style={{ minHeight: "100vh" }}>
-        <div className="d-flex flex-wrap gap-4 justify-content-center">
-          {products.map(product => (
-            <ItemCard key={product.id} product={product} />
-          ))}
-        </div>
+    <div className="container p-5" style={{ minHeight: "100vh" }}>
+      <div className="d-flex flex-wrap gap-4 justify-content-center">
+        {filtered.map(product => (
+          <ItemCard key={product.id} product={product} />
+        ))}
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default ItemsList
+export default ItemsList;
