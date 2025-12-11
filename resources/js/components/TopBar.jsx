e14e6deac3a25613417437fd53f79c36f76eae40
detossj <jorgerubilarssj@gmail.com>
2025-12-11 13:08:32 -0300

    changed NavLink in TopBar.jsx

--- resources/js/components/TopBar.jsx
+++ resources/js/components/TopBar.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import logo from '../../assets/images/logo.webp';
 import './TopBar.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 const TopBar = () => {
+  
+  const location = useLocation();
+
+  const activeMenuPaths = [
+    "/",
+    "/acompanamientos",
+    "/bebidas",
+    "/pizzas",
+    "/hamburguesas",
+    "/postres",
+    "/extras"
+  ];
+
+  const isMenuActive = activeMenuPaths.includes(location.pathname);
+
   return (
     <div className="py-2" style={{borderBottom: "1px solid #e5e5e5"}}>
       <div className="container d-flex justify-content-between align-items-center">
@@ -15,9 +30,7 @@ const TopBar = () => {
 
             <NavLink
               to="/"
-              className={({ isActive }) =>
-                isActive ? "topbar-item active" : "topbar-item"
-              }
+              className={isMenuActive ? "topbar-item active" : "topbar-item"}
             >
               <i className="fa-solid fa-pizza-slice me-1"></i> MENÚ
             </NavLink>
