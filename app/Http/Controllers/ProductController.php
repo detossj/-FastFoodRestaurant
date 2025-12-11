c84e8acfd2f1e0a75330243d38883fd52a7511e4
detossj <jorgerubilarssj@gmail.com>
2025-12-11 18:18:50 -0300

    deleted productsByCategory function and added allProducts function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,12 +7,10 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function productsByCategory($categoryId) {
 
-        $data = Product::where('category_id', $categoryId)->get();
-
-        if ($data->isEmpty()) return response()->json(['message' => 'Categoría no encontrada'], 404);
-
-        return response()->json($data, 200);
+    public function allProducts()
+    {
+        return response()->json(Product::all(), 200);
     }
+
 }
